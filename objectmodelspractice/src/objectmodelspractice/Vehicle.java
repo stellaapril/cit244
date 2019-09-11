2849f8e140b56c80856c4ec29e53fce7940b6f55
Chen <sitian.chen@ccac.org>
2019-09-11 15:58:54 -0400

    cit244 09/11 carmodel

---
+++ objectmodelspractice/src/objectmodelspractice/Vehicle.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectmodelspractice;
+
+/**
+ *
+ * @author sitian.chen
+ */
+public class Vehicle {
+    //public variables
+    public String vin;
+    public String color;
+    public String brand;
+    public String makemodel;
+    public int totalkm;
+    public int year;
+    
+    //safety variables
+    public boolean hasAirbagornot;
+    
+    //private variables
+    private boolean isEnginerunning;
+    private boolean checkLightstatus;
+    private double speed;
+    
+    
+    
+    public void startCar(){
+        isEnginerunning = true;
+    }//close startCar
+    
+    public void stopCar(){
+        isEnginerunning = false;
+    }//close stopCar
+    
+    
+    public double accelerate(int increasemph){
+        speed = speed+increasemph;
+        return speed;
+    }//close method
+    
+    public double deaccelerate(int decreasemph){
+        speed = speed+decreasemph;
+        return speed;
+    }//close method
+    
+    public boolean checkLightstatus(){
+        if(checkLightstatus == true){
+            System.out.println("Light is on");
+        }else{
+            System.out.println("Light is not on");
+        }
+        return checkLightstatus;
+    }//close method
+    
+}
