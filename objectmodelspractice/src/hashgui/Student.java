ce86ae9cb1824b2a3ea2d2488af357cfa8887392
Chen <sitian.chen@ccac.org>
2019-11-06 15:42:42 -0500

    1106

---
+++ objectmodelspractice/src/hashgui/Student.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashgui;
+
+/**
+ *
+ * @author sitian.chen
+ */
+public class Student {
+     private String number;
+    private String name;
+    
+    public String getNumber(){
+        return number;
+    }
+    
+    public void setNumber(String n){
+        this.number=n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String na){
+        this.name=na;
+    }
+    
+}
