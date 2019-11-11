53940f33954673c2a24ffc9f9f33b946c4a44114
Chen <sitian.chen@ccac.org>
2019-11-11 15:49:04 -0500

    DH

--- objectmodelspractice/src/diffiepractice/DHKeyAgreement.java
+++ objectmodelspractice/src/diffiepractice/DHKeyAgreement.java
@@ -5,20 +5,37 @@
  */
 package diffiepractice;
 
+import java.security.AlgorithmParameters;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.Cipher;
 import javax.crypto.KeyAgreement;
+import javax.crypto.ShortBufferException;
 import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+
+import java.io.*;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.*;
+import java.security.interfaces.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import javax.crypto.interfaces.*;
+import com.sun.crypto.provider.SunJCE;
 
 /**
  *
  * @author sitian.chen
  */
 public class DHKeyAgreement {
+
+
     private DHKeyAgreement(){
         
     }
@@ -69,6 +86,77 @@ public class DHKeyAgreement {
         PublicKey tomPubKey = jerryKeyFac.generatePublic(x509KeySpec);
         System.out.println("Jerry: Execute PHASE1 ...");
         jerryKeyAgree.doPhase(tomPubKey, true);
+        
+        System.out.println("Tom: Execute PHASE1 ...");
+        tomKeyAgree.doPhase(jerryPubKey,true);
+        
+        //jerry and tom have completed the dh key
+        
+        try{
+            byte[] jerrySharedSecret = jerryKeyAgree.generateSecret();
+            int jerryLen = jerrySharedSecret.length;
+            byte[] tomSharedSecret = new byte[jerryLen];
+            int tomLen;
+        }catch (ShortBufferException e) {
+            System.out.println(e.getMessage());
+        } // provide output buffer of required size
+         
+        tomLen = tomKeyAgree.generateSecret(tomSharedSecret, 0);
+        System.out.println("Jerry secret: " +
+                toHexString(jerrySharedSecret));
+        System.out.println("Tom secret: " +
+                toHexString(tomSharedSecret));
+        if (!java.util.Arrays.equals(jerrySharedSecret, tomSharedSecret))
+            throw new Exception("Shared secrets differ");
+        System.out.println("Shared secrets are the same");
+         
+        
+        System.out.println("Use shared secret as SecretKey object ...");
+        SecretKeySpec tomAesKey = new SecretKeySpec(tomSharedSecret, 0, 16, "AES");
+        SecretKeySpec jerryAesKey = new SecretKeySpec(jerrySharedSecret, 0, 16, "AES");
+        
+        
+        Cipher tomCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        tomCipher.init(Cipher.ENCRYPT_MODE, tomAesKey);
+        byte[] cleartext = "This is just an example".getBytes();
+        byte[] ciphertext = tomCipher.doFinal(cleartext);
+        
+        byte[] encodedParams = tomCipher.getParameters().getEncoded();
+        
+        
+        AlgorithmParameters aesParams = AlgorithmParameters.getInstance("AES");
+        aesParams.init(encodedParams);
+        Cipher jerryCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        jerryCipher.init(Cipher.DECRYPT_MODE, jerryAesKey, aesParams);
+        byte[] recovered = jerryCipher.doFinal(ciphertext);
+        if (!java.util.Arrays.equals(cleartext, recovered))
+            throw new Exception("AES in CBC mode recovered text is " +
+                    "different from cleartext");
+        System.out.println("AES in CBC mode recovered text is same as cleartext");
+        
 
     }
+    
+     private static void byte2hex(byte b, StringBuffer buf) {
+        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        int high = ((b & 0xf0) >> 4);
+        int low = (b & 0x0f);
+        buf.append(hexChars[high]);
+        buf.append(hexChars[low]);
+    }
+    
+    private static String toHexString(byte[] block) {
+        StringBuffer buf = new StringBuffer();
+        int len = block.length;
+        for (int i = 0; i < len; i++) {
+            byte2hex(block[i], buf);
+            if (i < len-1) {
+                buf.append(":");
+            }
+        }
+        return buf.toString();
+    }
 }
+
+
